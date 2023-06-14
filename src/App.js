fc8c2be4195160c5432d53ab2759670af8ef4859
raimanish9894 <84874096+raimanish9894@users.noreply.github.com>
2023-06-14 20:46:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import SearchPage from "./components/SearchPage";
+import Map from "./components/Map";
+
+
+
+function App() {
+  return (
+    <div>
+      <Map />
+      {/* <SearchPage classname ="searchPage"/> */}
+    </div>
+  );
+}
+
+export default App;
